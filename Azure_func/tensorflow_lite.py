f0158c6da98b7487725bd9327521612d1945bac2
Christian <christianichebi@gmail.com>
2024-12-15 23:04:54 +0100

    Modified file

--- Azure_func/tensorflow_lite.py
+++ Azure_func/tensorflow_lite.py
@@ -70,15 +70,10 @@ input_index = interpreter.get_input_details()[0]['index']
 output_index = interpreter.get_output_details()[0]['index']
 
 
-# In[23]:
-
-
 interpreter.set_tensor(input_index, X)
 interpreter.invoke()
 preds = interpreter.get_tensor(output_index)
 
-
-# In[24]:
 
 
 preds
@@ -86,26 +81,14 @@ preds
 
 # ### Removing Tensorflow dependencies
 
-# In[8]:
-
-
 from PIL import Image
-
-
-# In[11]:
 
 
 img_path = "../week8/test/pants/1b5f2882-e33e-4efc-b469-bcf87b9f53ed.jpg"
 
 
-# In[12]:
-
-
 with Image.open(img_path) as img:
     img = img.resize((299, 299), Image.NEAREST)
-
-
-# In[13]:
 
 
 # Get the code for preprocessing images from keras github repo
@@ -115,50 +98,26 @@ def preprocess_input(x):
     return x
 
 
-# In[34]:
-
-
 X = np.array([img], dtype='float32')
 
 X = preprocess_input(X)
 
-
-# In[35]:
-
-
 interpreter.set_tensor(input_index, X)
 interpreter.invoke()
 preds = interpreter.get_tensor(output_index)
-
-
-# In[36]:
-
 
 dict(zip(classes, preds[0]))
 
 
 # ### Alternative way of doing this
 
-# In[38]:
-
-
 get_ipython().system('pip install keras-image-helper')
-
-
-# In[51]:
 
 
 pip install ai-edge-litert
 
-
-# In[53]:
-
-
 from ai_edge_litert.interpreter import Interpreter
 from keras_image_helper import create_preprocessor
-
-
-# In[54]:
 
 
 interpreter = Interpreter(model_path='../models/clothing-model.tflite')
@@ -168,34 +127,18 @@ input_index = interpreter.get_input_details()[0]['index']
 output_index = interpreter.get_output_details()[0]['index']
 
 
-# In[41]:
-
-
 preprocessor = create_preprocessor('xception', target_size=(299, 299))
 
 
-# In[46]:
+#img_path = "../week8/test/pants/1b5f2882-e33e-4efc-b469-bcf87b9f53ed.jpg"
 
 
-img_path = "../week8/test/pants/1b5f2882-e33e-4efc-b469-bcf87b9f53ed.jpg"
+def predict(url):
+    X = preprocessor.from_path(url)
+
+    interpreter.set_tensor(input_index, X)
+    interpreter.invoke()
+    preds = interpreter.get_tensor(output_index)
 
 
-# In[47]:
-
-
-X = preprocessor.from_path(img_path)
-
-
-# In[48]:
-
-
-interpreter.set_tensor(input_index, X)
-interpreter.invoke()
-preds = interpreter.get_tensor(output_index)
-
-
-# In[49]:
-
-
-dict(zip(classes, preds[0]))
-
+    return dict(zip(classes, preds[0]))
