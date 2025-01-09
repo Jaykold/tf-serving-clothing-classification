0f5e61cbd0ff7e139723c95d0b234a50e9c9c267
Familia WorshipArc <christianichebi@gmail.com>
2025-01-09 18:46:24 +0100

    modified this files

--- tf_serving/image-model.dockerfile
+++
@@ -1,5 +0,0 @@
-FROM tensorflow/serving:2.14.0
-
-# Copy the model to the model directory
-COPY clothing-model /models/clothing-model/1
-ENV MODEL_NAME=clothing-model
--- tf_serving/test.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-endpoint = 'http://localhost:8001/predict'
-url = "https://bit.ly/al-gaib"
-data = {'url':  url}
-
-response = requests.post(endpoint, json=data).json()
-print(response)
